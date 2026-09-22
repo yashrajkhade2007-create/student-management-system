59650e6debe6cee37120703f3f9641cf04e4ac3d
yashrajkhade2007-create <yashrajkhade2007@gmail.com>
2026-09-23 00:38:45 +0530

    Update database connection path in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./config/db');
+const connectDB = require('./db');
 const studentRoutes = require('./routes/studentRoutes');
 
 connectDB();
